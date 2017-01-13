6771ffe41bec40285c3df38db932f17133e4d2b0
Florencio Vieira Filho <fvfilho@stefanini.com>
2017-01-13 10:12:08 -0200

    Imagem/Configurações

--- src/br/com/serpente/Constantes.java
+++ src/br/com/serpente/Constantes.java
@@ -3,23 +3,23 @@ package br.com.serpente;
 import java.awt.Color;
 
 public interface Constantes {
-	public static final byte TOTAL_FASES = 2;
+	public static final byte TOTAL_FASES = 10;
 
-	public static final byte TOTAL_POR_FASE = 5;
+	public static final byte TOTAL_POR_FASE = 1;
 
-	public static final byte LADO_QUADRADO = 10;
+	public static final byte LADO_QUADRADO = 15;
 
 	public static final Color COR_CABECA = Color.BLUE;
 
 	public static final Color COR_CORPO = Color.BLACK;
 
-	public static final short INTERVALO_MOVIMENTO = 200;
+	public static final short INTERVALO_MOVIMENTO = 110;
 
-	public static final short INTERVALO_DECREMENTO = 50;
+	public static final short INTERVALO_DECREMENTO = 10;
 
 	public static final int COMPRIMENTO_SERPENTE = TOTAL_FASES * TOTAL_POR_FASE;
 
-	public static final short LARGURA_JANELA = 500;
+	public static final short LARGURA_JANELA = 600;
 
-	public static final short ALTURA_JANELA = 400;
+	public static final short ALTURA_JANELA = 500;
 }
