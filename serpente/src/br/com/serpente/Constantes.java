a184ba1a6ac425f698d7f508f6abbb6f9d822c38
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-05-16 23:58:23 -0300

    Configuração

--- serpente/src/br/com/serpente/Constantes.java
+++ serpente/src/br/com/serpente/Constantes.java
@@ -9,9 +9,9 @@ public class Constantes {
 	
 	public static final boolean INICIAR_MODO_DEMO = true;
 	
-	public static final byte TOTAL_FASES = 1;
+	public static final byte TOTAL_FASES = 10;
 
-	public static final short TOTAL_POR_FASE = 500;
+	public static final short TOTAL_POR_FASE = 1;
 
 	public static final byte LADO_QUADRADO = 10;
 
@@ -19,9 +19,9 @@ public class Constantes {
 
 	public static final Color COR_CORPO = Color.BLACK;
 
-	public static final short INTERVALO_MOVIMENTO = 500;
+	public static final short INTERVALO_MOVIMENTO = 110;
 
-	public static final short INTERVALO_DECREMENTO = 0;
+	public static final short INTERVALO_DECREMENTO = 10;
 
 	public static final int COMPRIMENTO_SERPENTE = TOTAL_FASES * TOTAL_POR_FASE;
 
